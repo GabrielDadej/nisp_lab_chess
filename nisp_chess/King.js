c0e549611607642816a96e6300e1dd108567e50c
unknown <40469@student.atar.edu.pl>
2026-05-26 10:37:45 +0200

    dodano skoczka

--- nisp_chess/King.js
+++ nisp_chess/King.js
@@ -1,12 +1,16 @@
-// Klasa reprezentująca króla
 class King {
     constructor() {
         this.type = "KING";
     }
 
     validateMove(move) {
-        
-        // dokoncz implementacje
+        const rowDiff = Math.abs(move.toRow - move.fromRow);
+        const colDiff = Math.abs(move.toCol - move.fromCol);
+
+        // Ruch maksymalnie o 1 pole w każdą stronę, ale figura musi zmienić pozycję
+        if (rowDiff <= 1 && colDiff <= 1 && (rowDiff > 0 || colDiff > 0)) {
+            return true;
+        }
         return false;
     }
 }
--- nisp_chess/Knight.js
+++ nisp_chess/Knight.js
@@ -1,12 +1,13 @@
-// Klasa reprezentująca skoczka / konika
 class Knight {
     constructor() {
         this.type = "KNIGHT";
     }
 
     validateMove(move) {
-        
-        // dokoncz implementacje
+        const xDiff = Math.abs(move.destinationX - move.sourceX);
+        const yDiff = Math.abs(move.destinationY - move.sourceY);
+        if ((xDiff === 2 && yDiff === 1) || (xDiff === 1 && yDiff === 2)) 
+            return true;
         return false;
     }
 }
